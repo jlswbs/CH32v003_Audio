923c99539a515009e44ebffda635707810434a88
jlswbs <jlswbs@users.noreply.github.com>
2025-08-27 22:59:03 +0200

    Add files via upload

---
+++ Osc_Logistic/Osc_Logistic.ino
@@ -0,0 +1,56 @@
+// Logistic equation chaotic oscillator - pin PD0 audio PWM output //
+
+#include <HardwareTimer.h>
+
+HardwareTimer Timer(TIM2);
+
+#define SAMPLE_RATE 44100
+
+  float a = 1.3f;
+  float x = 0.15f;
+
+
+void timerHandler(void) {
+
+	Timer.pause();
+
+  x = -a * (1.0f - x) * x / (fabs(x) + 0.01f);
+
+	TIM1->CH1CVR = 92 * x;
+  
+  Timer.resume();
+
+}
+
+void PWM_init() {
+
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_TIM1;
+	GPIOD->CFGLR &= ~(0xf<<(4*0));
+	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_Mode_AF_PP)<<(4*0);
+	RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
+	RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
+	TIM1->PSC = 0x0000;
+	TIM1->ATRLR = 255;
+	TIM1->SWEVGR |= TIM_UG;
+	TIM1->CCER |= TIM_CC1NE | TIM_CC1NP;
+	TIM1->CCER |= TIM_CC4E | TIM_CC4P;
+	TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1;
+	TIM1->CH1CVR = 128;
+	TIM1->BDTR |= TIM_MOE;
+	TIM1->CTLR1 |= TIM_CEN;
+
+}
+
+void setup() {
+
+  PWM_init();
+	
+	Timer.setOverflow(SAMPLE_RATE, HERTZ_FORMAT);
+  Timer.attachInterrupt(timerHandler);
+  Timer.resume();
+
+}
+
+void loop() {
+  
+}
