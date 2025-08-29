c5e892ce7d40095e5a9301cae4b3a75fd42ac7e7
jlswbs <jlswbs@users.noreply.github.com>
2025-08-29 15:57:13 +0200

    Add files via upload

---
+++ Byte_Beat/Byte_Beat.ino
@@ -0,0 +1,56 @@
+// Byte-Beat generator - pin PD0 audio PWM output //
+
+#include <HardwareTimer.h>
+
+HardwareTimer Timer(TIM2);
+
+#define SAMPLE_RATE 8000
+
+  long t;
+
+void timerHandler(void) {
+
+	Timer.pause();
+
+  uint8_t out = t * (t<<2 ^ t>>10) - (t | t>>9);
+
+  t++;
+
+  TIM1->CH1CVR = out;
+  
+  Timer.resume();
+
+}
+
+void PWM_init() {
+
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_TIM1;
+	GPIOD->CFGLR &= ~(0xf<<(4*0));
+	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_Mode_AF_PP)<<(4*0);
+	RCC->APB2PRSTR |= RCC_APB2Periph_TIM1;
+	RCC->APB2PRSTR &= ~RCC_APB2Periph_TIM1;
+	TIM1->PSC = 0x0000;
+	TIM1->ATRLR = 255;
+	TIM1->SWEVGR |= TIM_UG;
+	TIM1->CCER |= TIM_CC1NE | TIM_CC1NP;
+	TIM1->CCER |= TIM_CC4E | TIM_CC4P;
+	TIM1->CHCTLR1 |= TIM_OC1M_2 | TIM_OC1M_1;
+	TIM1->CH1CVR = 128;
+	TIM1->BDTR |= TIM_MOE;
+	TIM1->CTLR1 |= TIM_CEN;
+
+}
+
+void setup() {
+
+	PWM_init();
+	
+	Timer.setOverflow(SAMPLE_RATE, HERTZ_FORMAT);
+  Timer.attachInterrupt(timerHandler);
+  Timer.resume();
+
+}
+
+void loop() {
+
+}
